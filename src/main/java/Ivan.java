7d0717718c86df0d7e726de10d4df8372ee52e0b
Ivan Rogov <rogov.vanyusha@list.ru>
2020-03-25 12:40:10 +0300

    Добавлено 2 файла

--- src/main/java/Ivan.java
+++ src/main/java/Ivan.java
@@ -1,5 +1,34 @@
 public class Ivan {
     public static void main(String[] args) {
-        System.out.println("Ivan");
+
+        int x = Math.mult(5, 6);
+        int u = Math.summ(4, 7);
+        System.out.println(x);
+        System.out.println(u);
+
+        Math a = new Math();
+        System.out.println(a.x);
+        Math.x =22;
+        System.out.println(a.x);
+        Math b=new Math();
+        System.out.println(b.x);
+
+
+        //площадь круга
+        System.out.println(Math.roundsArea(5));
+
+        //площадь треугольника
+        System.out.println(Math.threeGonArea(3,4));
+
+
+
+
+
+
+
+
+
     }
+
+
 }
---
+++ src/main/java/Math.java
@@ -0,0 +1,36 @@
+import javafx.print.PageLayout;
+
+public class Math {
+
+    public static int x = 16;
+
+
+
+    public static int summ (int a, int b){
+        return a+b;
+    }
+
+    public static int mult (int a, int b){
+        return a*b;
+    }
+
+    public static double r;
+    public static double A;
+    public static final double Pl = 3.14;
+
+
+    public static double roundsArea (double r){
+        return (r*r)*Pl;
+    }
+
+    public static double threeGonArea (double A, double B){
+        return (A*B)*0.5;
+    }
+
+
+
+
+
+
+
+}
