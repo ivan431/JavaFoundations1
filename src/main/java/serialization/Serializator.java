2fe2182f2e9bbef6cb2f8a33d187380c772c9728
Ivan Rogov <rogov.vanyusha@list.ru>
2020-04-28 23:03:32 +0300

    Это сохранялка текста +переписанные между собой файлы

--- src/main/java/serialization/Serializator.java
+++ src/main/java/serialization/Serializator.java
@@ -1,6 +1,7 @@
 package serialization;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Serializator {
     public static void main(String[] args) {
@@ -12,7 +13,7 @@ public class Serializator {
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
 
              objectOutputStream.writeObject(user);
-             objectOutputStream.close();
+             objectOutputStream.flush();
 
 
         } catch (IOException ex) {
@@ -23,20 +24,48 @@ public class Serializator {
 
 
         File file = new File("Save1.txt");
-        try { PrintWriter printWriter = new PrintWriter(file);
+        String str = new Scanner(System.in).next();
+        try {
+            FileWriter fileWriter = new FileWriter(file, true);
 
-            printWriter.println("vgvgvgv11");
-            printWriter.close();
+            fileWriter.write(str);
+            fileWriter.flush();
         } catch (IOException ex) {
             ex.printStackTrace(System.out);
         }
 
 
 
+        try(FileInputStream fileInputStream1=new FileInputStream("Save1.txt");
+            FileOutputStream fileOutputStream1=new FileOutputStream("Save3.txt"))
+        {
+            byte[] buffer = new byte[fileInputStream1.available()];//засовываю содержимое в буфер
+            // считываю буфер (в скобках указываю, что буду читать!
+            fileInputStream1.read(buffer);
+            // записываю из буфера в файл
+            fileOutputStream1.write(buffer);
+
+            FileInputStream fileInputStream2 = new FileInputStream("Save3.txt");
+            FileOutputStream fileOutputStream2 = new FileOutputStream("Save1.txt");
+
+            byte[] buffer1 = new byte[fileInputStream1.available()];
+            fileInputStream2.read(buffer1);
+            fileOutputStream2.write(buffer1);
+
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
+
+/*
+
         try {
             FileOutputStream fileOutputStream = new FileOutputStream("Save2.txt");
-            fileOutputStream.write(45454);
-            fileOutputStream.write(412121);
+            String thisString = "Эта строка лежит здесь";
+            //if(fileOutp)
+
+            fileOutputStream.flush();
+
 
         } catch (IOException ex) {
             ex.printStackTrace(System.out);
@@ -64,6 +93,8 @@ public class Serializator {
         } catch (ClassNotFoundException ex){
             ex.printStackTrace(System.out);
         }
+
+ */
 
 
 
